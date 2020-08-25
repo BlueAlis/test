0e7858d656f46d268ba3757a6fb51b5830fb93e5
BlueAlis <nattaphatblue.2545@gmail.com>
2020-08-26 00:15:40 +0700

    text

---
+++ blue-app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/po.png" width="500" height="100">
+    <p>
+    <a href="https://drive.google.com/file/d/13CkDsIUGb6zddYFynkVVfs132xKpHef2/view?usp=sharing" target="_blank" rel="noopener"><img src="./assets/PART 1 PROFILE.png" width="700" height="200"></a>.
+    </p>
+    <p>
+    <a href="https://drive.google.com/file/d/1zdmRl2mNdCBK3ndoqFDytSd_fXUWMMJB/view?usp=sharing" target="_blank" rel="noopener"><img src="./assets/PART 2.png" width="700" height="200"></a>.
+    </p>
+    <p>
+    <a href="https://drive.google.com/file/d/1Ii6fwRCvNlxOcw7LtDNgfXUZIjGG70oJ/view?usp=sharing" target="_blank" rel="noopener"><img src="./assets/PART3.png" width="700" height="200"></a>.
+    </p>
+    <User name="BlueAlis" bio="The strongest man in the world" />
+  </div>
+</template>
+
+<script>
+/*import User from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    User
+  }
+}*/
+</script>
+
+<style>
+#app {
+  background-image:url("./assets/universe-1598369694137-8787.jpg" );
+  /*background-repeat : repeat;*/
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 120px;*/
+  
+}
+</style>
